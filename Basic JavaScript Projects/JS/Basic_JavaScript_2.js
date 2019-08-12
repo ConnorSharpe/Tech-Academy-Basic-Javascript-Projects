818e23da54caf1183c0fa7394764b3bf4ece518b
ConnorSharpe <52796555+ConnorSharpe@users.noreply.github.com>
2019-08-12 15:51:37 -0700

    add concatenation experience

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_2.js
@@ -0,0 +1,13 @@
+
+function hFunction() {
+    var sentence = "This sentence";
+    sentence += " has been concatenated";
+    document.getElementById("heading").innerHTML = sentence;
+}
+
+
+function MyFirstFunction() {
+    var String = "This text is green";
+    var result = String.fontcolor("green");
+    document.getElementById("para").innerHTML = result;
+}
