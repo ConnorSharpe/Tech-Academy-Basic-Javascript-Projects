214f0d3057c4f78eff3504f498275aca90271f17
ConnorSharpe <52796555+ConnorSharpe@users.noreply.github.com>
2019-08-12 13:40:00 -0700

    Start new Javascript class

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,18 @@
+window.alert("Hello, World!");
+document.write("You are the best");
+
+var A = "This is a string";
+var A = A.fontcolor("green");
+document.write(A);
+window.alert(A);
+
+document.write("Dad always said, \"Why do you do the things that you do?\"");
+var B = "What is" + " your name?";
+var B = B.fontcolor("red");
+document.write(B);
+
+var C = "wartortle", D = "blastoise", E = "squirtle";
+var D = D.fontcolor("blue");
+document.write(D);
+
+document.write(33*54);
